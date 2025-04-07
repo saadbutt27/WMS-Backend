df876697f3b55defae6a72299c3ca166fae07ad7
Saad Nisar Butt <83548173+saadbutt27@users.noreply.github.com>
2025-04-07 13:10:20 +0500

    added base url health cehck api

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const routes = require('./routes');
-const { connectDB } = require('./config/database');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
+const { connectDB } = require("./config/database");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api', routes);
+// health check api
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "Server is running. Welcome to WMS API." });
+});
+
+app.use("/api", routes);
 
 connectDB();
 
